a597de6642a2ec2144fc98d687f7f734c2692397
JoseOgam <jotienoochieng@gmail.com>
2021-06-17 07:26:56 -0400

    counter App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@
 function App() {
   return (
     <div>
-     <h1>I missed You! oh My..</h1>
+      <h1>I missed You! oh My..</h1>
+      <h3>Counter App</h3>
+      <button>+1</button>
+      <button>-1</button>
     </div>
   );
 }
