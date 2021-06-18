be8acf2af427ae442028e028f98720fc4cc7f4b5
JoseOgam <jotienoochieng@gmail.com>
2021-06-18 07:00:15 -0400

    counter App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import  { useState } from 'react';
 function App() {
+  const [counter, setCounter] = useState(0)
+  const increment = () => setCounter(counter + 1)
+  const decrement = () => setCounter(counter - 1)
+  const reset = ()=>setCounter(0)
   return (
     <div>
       <h1>I missed You! oh My..</h1>
       <h3>Counter App</h3>
-      <button>+1</button>
-      <button>-1</button>
+      <h4>current count: {counter}</h4>
+      <button onClick={increment}>+1</button>
+      <button onClick={ decrement }>-1</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
